344555a5223882bd5de1d3db167bf1cde9206983
syntaxsizer <kakuma4never@gmail.com>
2016-05-28 18:51:35 +0300

    add calculater buttons and button ids

--- app/src/main/java/com/android/qusai/basic/MainActivity.java
+++ app/src/main/java/com/android/qusai/basic/MainActivity.java
@@ -2,12 +2,22 @@ package com.android.qusai.basic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button addition, subtra, divide, mulyi;
+    private TextView result;
+    private EditText firsttextfield,secondtextfield;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+    private init(){
+        addition =(Button) findViewById(R.layout,addition);
+
     }
 }
